85f22fd013def5af4f64b851f27c289dd6d33bce
Keith P <aerpaw.msu@gmail.com>
2020-11-06 11:38:47 -0500

    Initial

---
+++ build/spgw_c/build/sgwc/CMakeFiles/SGWC.dir/DependInfo.cmake
@@ -0,0 +1,51 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/aerpaw/openair-cn-cups/src/sgwc/sgwc_app.cpp" "/home/aerpaw/openair-cn-cups/build/spgw_c/build/sgwc/CMakeFiles/SGWC.dir/sgwc_app.cpp.o"
+  "/home/aerpaw/openair-cn-cups/src/sgwc/sgwc_config.cpp" "/home/aerpaw/openair-cn-cups/build/spgw_c/build/sgwc/CMakeFiles/SGWC.dir/sgwc_config.cpp.o"
+  "/home/aerpaw/openair-cn-cups/src/sgwc/sgwc_eps_bearer_context.cpp" "/home/aerpaw/openair-cn-cups/build/spgw_c/build/sgwc/CMakeFiles/SGWC.dir/sgwc_eps_bearer_context.cpp.o"
+  "/home/aerpaw/openair-cn-cups/src/sgwc/sgwc_pdn_connection.cpp" "/home/aerpaw/openair-cn-cups/build/spgw_c/build/sgwc/CMakeFiles/SGWC.dir/sgwc_pdn_connection.cpp.o"
+  "/home/aerpaw/openair-cn-cups/src/sgwc/sgwc_procedure.cpp" "/home/aerpaw/openair-cn-cups/build/spgw_c/build/sgwc/CMakeFiles/SGWC.dir/sgwc_procedure.cpp.o"
+  "/home/aerpaw/openair-cn-cups/src/sgwc/sgwc_s11.cpp" "/home/aerpaw/openair-cn-cups/build/spgw_c/build/sgwc/CMakeFiles/SGWC.dir/sgwc_s11.cpp.o"
+  "/home/aerpaw/openair-cn-cups/src/sgwc/sgwc_s5s8.cpp" "/home/aerpaw/openair-cn-cups/build/spgw_c/build/sgwc/CMakeFiles/SGWC.dir/sgwc_s5s8.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BSTRLIB_CAN_USE_IOSTREAM=1"
+  "BSTRLIB_CAN_USE_STL=1"
+  "BSTRLIB_THROWS_EXCEPTIONS=1"
+  "CMAKER"
+  "CMAKE_BUILD_TYPE=\"Debug\""
+  "DISPLAY_LICENCE_INFO=0"
+  "LOG_OAI=1"
+  "PACKAGE_BUGREPORT=\"openaircn-user@lists.eurecom.fr\""
+  "PACKAGE_NAME=\"SPGW-C\""
+  "PACKAGE_VERSION=\"Branch: master Abrev. Hash: 5ca3839 Date: Thu Sep 19 10:55:33 2019 +0200\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/aerpaw/openair-cn-cups/src/pgwc"
+  "/home/aerpaw/openair-cn-cups/src/oai_spgwc"
+  "/home/aerpaw/openair-cn-cups/src/common"
+  "/home/aerpaw/openair-cn-cups/src/itti"
+  "/home/aerpaw/openair-cn-cups/src/common/msg"
+  "/home/aerpaw/openair-cn-cups/src/common/utils"
+  "/home/aerpaw/openair-cn-cups/src/gtpv2c"
+  "/home/aerpaw/openair-cn-cups/src/pfcp"
+  "/home/aerpaw/openair-cn-cups/src/sgwc"
+  "/home/aerpaw/openair-cn-cups/src/udp"
+  "/home/aerpaw/openair-cn-cups/src/../build/ext/spdlog/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
